90e40a6fa12d38454df095fcf746122f3b46dac8
Yuri Macedo <yuri_paulino@hotmail.com>
2024-03-19 21:43:32 -0300

    Aula 42 Microserviços(Nivelamento)

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum);
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private int ConvertToDecimal(string secondNumber) 
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsNumeric(string firstNumber) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
